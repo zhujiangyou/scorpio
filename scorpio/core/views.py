55c25a529b91ff7eb0240a8af759c36080016976
zjy <zhujiangyou88@163.com>
2019-05-23 19:57:49 +0800

    z

--- scorpio/core/views.py
+++ scorpio/core/views.py
@@ -176,12 +176,12 @@ def wechat_login(request):
                         # try:
                             # 在表中添加当前用户扫描某一个二维码的记录
 
-                            once_credit = OnlyOnceCredit.objects.get(
-                                pk=only_credit_id)
-                            UserScan.objects.create(
-                                user=user, credit=once_credit)
-                        except BaseException:
-                            return HttpResponse('Please scan again')
+                        once_credit = OnlyOnceCredit.objects.get(
+                            pk=only_credit_id)
+                        UserScan.objects.create(
+                            user=user, credit=once_credit)
+                        # except BaseException:
+                            # return HttpResponse('Please scan again')
 
                 if user.name and user.hotel_name:
                     user.credit += int(credit)
