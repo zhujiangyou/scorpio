59bca48e93a645772ca22e7ced3e116d64ecf736
zjy <zhujiangyou88@163.com>
2019-05-29 16:55:16 +0800

    z

--- scorpio/core/api.py
+++ scorpio/core/api.py
@@ -1,9 +1,14 @@
 from restapi import api, APIError
 from .models import *
+import requests
 @api
 def mini_login(code):
-
-    print(1111)
     print(code)
+    appid = 'wx8c822d6f747d1e6a'
+    secret = '2419917cec1c48a0e421a9e3513f754e'
 
-    return code
+    url = 'https://api.weixin.qq.com/sns/jscode2session?appid={0}&secret={1}&js_code={2}&grant_type=authorization_code'.format(appid, secret, code)
+    res = requests.get(url)
+    print(res)
+
+    # return code
