2d7b10ec71006f4b19ff44952d551055c4b8fa07
zjy <zhujiangyou88@163.com>
2019-06-03 15:01:54 +0800

    z

--- scorpio/core/admin.py
+++ scorpio/core/admin.py
@@ -8,7 +8,6 @@ admin.site.register(Event)
 admin.site.register(Food)
 admin.site.register(Credit)
 admin.site.register(History)
-
 admin.site.register(Favorite)
 admin.site.register(LastFood)
 admin.site.register(Lunch)
@@ -18,7 +17,13 @@ admin.site.register(LunchReservation)
 admin.site.register(OnlyOnceCredit)
 admin.site.register(UserScan)
 admin.site.register(TeaBreak)
-
 admin.site.register(UserFood)
+
+admin.site.register(AgendaDate)
+admin.site.register(Attach)
+admin.site.register(Agenda)
+admin.site.register(AgendaDetail)
+
+
 
 
