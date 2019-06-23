e7abee5b5a0e75d0fbbce46a78d91c5c10767d76
zjy <zhujiangyou88@163.com>
2019-06-23 12:02:19 +0800

    z

--- scorpio/core/api.py
+++ scorpio/core/api.py
@@ -21,8 +21,6 @@ def mini_login(status, code, userInfo):
     print('res',res)
     print('status',status)
 
-    userInfo = json.loads(userInfo)
-
     event = Event.objects.all().first()
     if status == 'firstLogin':
         if user:
